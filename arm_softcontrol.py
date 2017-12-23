04b811248b981a2e0997adfdcb451948c4955680
Aditya Agarwal <aditya_ag2301@yahoo.in>
2017-12-23 11:08:12 +0530

    Update arm_softcontrol.py

--- arm_softcontrol.py
+++ arm_softcontrol.py
@@ -8,13 +8,13 @@ grip=3
 
 def SetPinAngle(pin,angle):
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(7,GPIO.OUT)
+    GPIO.setup(pin,GPIO.OUT)
     pwm=GPIO.PWM(pin,50)
     duty=angle/18 + 2
-    GPIO.output(7,True)
+    GPIO.output(pin,True)
     pwm.ChangeDutyCycle(duty)
     sleep(2)
-    GPIO.output(7,False)
+    GPIO.output(pin,False)
     pwm.ChangeDutyCycle(0)
 
 
