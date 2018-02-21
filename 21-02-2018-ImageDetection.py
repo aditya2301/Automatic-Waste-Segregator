3fb7656d343064b03821758cba5d8eb16352301c
aditya2301 <aditya_ag2301@yahoo.in>
2018-02-21 05:49:11 +0000

    updated on 21-02-2018

--- 21-02-2018-ImageDetection.py
+++ 21-02-2018-ImageDetection.py
@@ -13,7 +13,26 @@ def imageSubtract(img):
     #y=cv2.equalizeHist(y)
     return v
 
+def contourWindow(c):
+    x=422
+    y=512
+    vertical=int(x/2)
+    horizontal=int(y/2)
+    
+    quadrant= np.zeros((512,422),np.uint8)
+    cv2.line(quadrant,(vertical,0),(vertical,y),(255,0,0),3)#vertical line
+    cv2.line(quadrant,(0,horizontal),(x,horizontal),(159,73,14),3)
+    cv2.drawContours(quadrant, [c] , 0, (126,76,0), -1)
+    cv2.imshow("quadrant",quadrant)
+    print("contour function called")
+    return 1
+
 def  imageProcessing():
+    x=422
+    y=512
+    vertical=int(x/2)
+    horizontal=int(y/2)
+    
     camera = PiCamera()
     camera.resolution = (512,512)
     #camera.zoom=(0.0,0.0,0.4,0.5)
@@ -24,6 +43,8 @@ def  imageProcessing():
     camera.sharpness=100
     rawCapture = PiRGBArray(camera, size=(512, 512))
 
+    is_quadrantWindow_open=0
+    sleep_time=0
     first_time=0
     frame_buffer=0
     counter=0
@@ -41,13 +62,21 @@ def  imageProcessing():
                continue
             os.system("clear")
             refImg=frame.array
+            #y,x=refImg.shape
+            
+            refImg=refImg[0:512,0:512-90]
             refThresh=imageSubtract(refImg)
             first_time=1
             frame_buffer=0
 
 
         frame_buffer+=1
+        
+        
+        
+        
         image = frame.array
+        image=image[0:512,0:512-90]
         cv2.imshow("Foreground", image)
         key = cv2.waitKey(1)
         rawCapture.truncate(0)
@@ -71,15 +100,42 @@ def  imageProcessing():
            
            cv2.rectangle(thresholded,(x,y),(x+w,y+h),(125,125,125),2)
            if cv2.contourArea(c)>300 and len(contours)<=5:
+              os.system("clear")
+              M=cv2.moments(c)
+              cY = int(M['m10']/M['m00'])
+              cX = int(M['m01']/M['m00'])
               print("Total contours found=",len(contours))
               print("Object detected with area = ",cv2.contourArea(c))
+              print("Object's X,Y=",cX,cY)
+
+              print("Object is located at = ",end="")
+              if cX<horizontal and cY<vertical:
+                  print("1st Quadrant")
+              elif cX<horizontal and cY>vertical:
+                  print("2nd Quadrant")
+              elif cX>horizontal and cY<vertical:
+                  print("3rd Quadrant")
+              elif cX>horizontal and cY>vertical:
+                  print("4th Quadrant")
+              elif cX==horizontal and cY<vertical:
+                  print("Between 1 and 3rd Quadrant")
+              elif cX==horizontal and cY>vertical:
+                  print("Between 2 and 4th Quadrant")
+              elif cX<horizontal and cY==vertical:
+                  print("Between 1 and 2nd Quadrant")
+              elif cX>horizontal and cY==vertical:
+                  print("Between 3 and 4th Quadrant")
+              else:
+                  print("Right at the centre !")
+
               
-              #print("Object detected with area = ",cv2.contourArea(c))
            
            cv2.imshow("Threshold",thresholded)
            if frame_buffer%40==0:
               refImg=image
+              #refImg=refImg[0:x,0:y-90]
               refThresh=imageSubtract(refImg)
+              os.system("clear")
               print("Refrence Image changed")
            if key == ord('q'):
                camera.close()
@@ -87,7 +143,8 @@ def  imageProcessing():
                break
            #print(frame_buffer)
         except Exception as e:
-           pass
+            print(e)
+            pass
 
 if __name__ == "__main__" :
    imageProcessing()
