ab2d420da1d12ac9019bf4b6f394004f03495214
Boudhayan Dev <dev.dibyo@gmail.com>
2018-03-05 00:27:02 +0530

    added the socket server/client code

---
+++ 02-03-2018 Final Integrated/final3/socketClient.py
@@ -0,0 +1,31 @@
+import socket  ,sys ,time            
+
+# Create a socket object
+s = socket.socket()         
+
+# Define the port on which you want to connect
+port = 80              
+
+# connect to the server on local computer
+s.connect(('104.196.154.179 ', port))
+
+
+f=open("7.jpeg","rb")
+data=f.read()
+f.close()
+length=str(len(data))
+s.send(bytes(length,"utf-8"))
+
+status=s.recv(10)
+print("Length Received status - "+str(status.decode("utf-8")))
+
+f=open("7.jpeg","rb")
+data=f.read(1)
+while d:
+	s.send(data)
+	data=f.read(1)
+f.close()
+
+binFlag=s.recv(20)
+print("Left/Right side of the bin - ",str(binFlag.decode("utf-8")))
+s.close()
