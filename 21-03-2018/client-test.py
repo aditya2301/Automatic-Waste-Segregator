4c858d970672bcd8b98b1389515cdb04335626e0
Edward Kenway <aditya_ag2301@yahoo.in>
2018-04-19 12:24:40 +0530

    19/4

--- 21-03-2018/client-test.py
+++ 21-03-2018/client-test.py
@@ -4,7 +4,7 @@ s = socket.socket()             # Create a socket object
 #host = input("enter your server IP address")    # Get local machine name
 port = 60000                 # Reserve a port for your service.
 #
-s.connect(("192.168.2.8", port))
+s.connect(("192.168.2.4", port))
 s.send("Hello server!")
 
 data = s.recv(2)
