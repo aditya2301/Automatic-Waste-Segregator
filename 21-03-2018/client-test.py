3bbda5d1fdb7a9a2e3bf1320529eb5741c6cd113
Boudhayan Dev <dev.dibyo@gmail.com>
2018-04-19 12:16:53 +0530

    added a client test file

---
+++ 21-03-2018/client-test.py
@@ -0,0 +1,12 @@
+import socket                   # Import socket module
+
+s = socket.socket()             # Create a socket object
+#host = input("enter your server IP address")    # Get local machine name
+port = 60000                 # Reserve a port for your service.
+#
+s.connect(("192.168.2.8", port))
+s.send("Hello server!")
+
+data = s.recv(2)
+s.close()
+print(data.decode("utf-8"))
