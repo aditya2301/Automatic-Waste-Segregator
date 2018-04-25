91dac533974fed7cb06f2db80c58ff024b2353da
Edward Kenway <aditya_ag2301@yahoo.in>
2018-04-25 14:56:38 +0530

    upload lcd

--- lcd codes/run_lcd.py
+++ lcd codes/run_lcd.py
@@ -2,22 +2,27 @@ import lcd
 import time
 
  while True:
+    # Send some text
+    lcd.lcd_string("Automatic Waste ",LCD_LINE_1)
+    lcd.lcd_string("   Segregation  ",LCD_LINE_2)
  
+    time.sleep(3)
+      
     # Send some test
-    lcd.lcd_string("Electronics Hub ",LCD_LINE_1)
-    lcd.lcd_string("    Presents    ",LCD_LINE_2)
+    lcd.lcd_string("Object Detected!",LCD_LINE_1)
+    time.sleep(1)
+    lcd.lcd_string("                ",LCD_LINE_1)
+    lcd.lcd_string("Sorting         ",LCD_LINE_2)
     
     time.sleep(3) # 3 second delay
  
     # Send some text
-    lcd.lcd_string("Rasbperry Pi",LCD_LINE_1)
-    lcd.lcd_string("16x2 LCD Test",LCD_LINE_2)
+    lcd.lcd_string("Object is ",LCD_LINE_1)
+    if binDir=='l':
+        lcd.lcd_string(" Biodegradable ",LCD_LINE_2)
+    else:
+        lcd.lcd_string("     Non-Bio    ",LCD_LINE_2)
 
     time.sleep(3) # 3 second delay
  
-    # Send some text
-    lcd.lcd_string("1234567890*@$#%&",LCD_LINE_1)
-    lcd.lcd_string("abcdefghijklmnop",LCD_LINE_2)
- 
-    time.sleep(3)
-      
+   
